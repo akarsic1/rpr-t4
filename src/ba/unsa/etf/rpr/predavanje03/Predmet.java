0846d4ee6790619b3f7fbdc467e1aa741e93d84c
Amel <akarsic1@etf.unsa.ba>
2018-11-05 23:07:44 +0100

    Klasa predmet urađena.

--- src/ba/unsa/etf/rpr/predavanje03/Predmet.java
+++ src/ba/unsa/etf/rpr/predavanje03/Predmet.java
@@ -5,4 +5,30 @@ import java.util.ArrayList;
 public abstract class Predmet {
     String subjectName ;
     ArrayList<Student> studentsListening = new ArrayList<Student>();
+    int brojEcts ;
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public ArrayList<Student> getStudentsListening() {
+        return studentsListening;
+    }
+
+    public void setStudentsListening(ArrayList<Student> studentsListening) {
+        this.studentsListening = studentsListening;
+    }
+
+    public int getBrojEcts() {
+        return brojEcts;
+    }
+
+    public void setBrojEcts(int brojEcts) {
+        this.brojEcts = brojEcts;
+    }
+
 }
