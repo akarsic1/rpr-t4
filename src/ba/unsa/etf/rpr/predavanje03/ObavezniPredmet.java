70d39d4e83d5489c3640eea7e25c0ccd7214526f
Amel <akarsic1@etf.unsa.ba>
2018-11-06 09:58:21 +0100

    ObavezniPredmet konstruktor.

--- src/ba/unsa/etf/rpr/predavanje03/ObavezniPredmet.java
+++ src/ba/unsa/etf/rpr/predavanje03/ObavezniPredmet.java
@@ -1,7 +1,16 @@
 package ba.unsa.etf.rpr.predavanje03;
 
+import java.util.ArrayList;
+
 public class ObavezniPredmet extends Predmet{
     boolean isRequired = true;
+
+    public ObavezniPredmet(String subjectName, ArrayList<Student> studentsListening, int brojEcts) {
+        this.isRequired = true;
+        this.subjectName = subjectName;
+        this.studentsListening = studentsListening;
+        this.brojEcts = brojEcts;
+    }
 
     public boolean isRequired() {
         return isRequired;
